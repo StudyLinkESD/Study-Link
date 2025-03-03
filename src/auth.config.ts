2b384e988c03dd4cc07248f54c4fcd27c6d238cd
Ethan Frot <contact@ethanfrot.com>
2025-03-03 18:40:14 +0100

    ✨Create signup route & send magic link

---
+++ src/auth.config.ts
@@ -0,0 +1,31 @@
+import { NextAuthConfig } from "next-auth";
+import Resend from "next-auth/providers/resend";
+
+export default {
+    providers: [
+        Resend({
+            apiKey: process.env.NEXT_PUBLIC_AUTH_RESEND_KEY,
+            from: "Dev <onboarding@resend.dev>",
+            async sendVerificationRequest({ identifier, url, provider }) {
+                console.log(url);
+                try {
+                    await fetch(`${process.env.NEXT_PUBLIC_API_URL}/resend`, {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json",
+                            Authorization: `Bearer ${provider.apiKey}`,
+                        },
+                        body: JSON.stringify({
+                            from: provider.from,
+                            to: identifier,
+                            subject: "Votre lien de connexion",
+                            url,
+                        }),
+                    });
+                } catch (error) {
+                    console.log(error);
+                }
+            },
+        }),
+    ],
+} satisfies NextAuthConfig;
