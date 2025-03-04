70e69a9cf6080835c526d6370b01e3be6b9f80a6
Ethan Frot <contact@ethanfrot.com>
2025-03-04 20:44:00 +0100

    🚨🎨Fix lint & change redirection url

--- src/auth.ts
+++ src/auth.ts
@@ -1,38 +1,38 @@
-import NextAuth from "next-auth";
-import authConfig from "@/auth.config";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import { prisma } from "@/lib/prisma";
+import NextAuth from 'next-auth';
+import authConfig from '@/auth.config';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import { prisma } from '@/lib/prisma';
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
-    adapter: PrismaAdapter(prisma),
-    session: { strategy: "jwt" },
-    pages: {
-        verifyRequest: `http://localhost:3000/auth/verify-request`,
+  adapter: PrismaAdapter(prisma),
+  session: { strategy: 'jwt' },
+  pages: {
+    verifyRequest: `${process.env.NEXT_PUBLIC_MAIN_URL}/verify-request`,
+  },
+  callbacks: {
+    async jwt({ token, user, account }) {
+      if (account && user) {
+        token.accessToken = account.access_token;
+        token.id = user.id;
+      }
+      return token;
     },
-    callbacks: {
-        async jwt({ token, user, account }) {
-            if (account && user) {
-                token.accessToken = account.access_token;
-                token.id = user.id;
-            }
-            return token;
-        },
-        async session({ session, token }) {
-            session.accessToken = token.accessToken as string;
-            session.user.id = token.id as string;
-            return session;
-        },
-        async redirect({ baseUrl }) {
-            return baseUrl;
-        },
+    async session({ session, token }) {
+      session.accessToken = token.accessToken as string;
+      session.user.id = token.id as string;
+      return session;
     },
-    events: {
-        async signIn(message) {
-            console.log("User signed in:", message.user);
-        },
-        async signOut(message) {
-            console.log("User signed out:", message);
-        },
+    async redirect({ baseUrl }) {
+      return baseUrl;
     },
-    ...authConfig,
+  },
+  events: {
+    async signIn(message) {
+      console.log('User signed in:', message.user);
+    },
+    async signOut(message) {
+      console.log('User signed out:', message);
+    },
+  },
+  ...authConfig,
 });
